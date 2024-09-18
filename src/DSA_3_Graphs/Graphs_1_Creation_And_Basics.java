17cf5dd1070f298fe158e959f8e270887cbaf0b9
OmNai442003 <omnayinor99@gmail.com>
2024-09-18 15:14:26 -0700

    Update Graphs_1_Creation_And_Basics.java
    Kruskal conflicts solved

--- src/DSA_3_Graphs/Graphs_1_Creation_And_Basics.java
+++ src/DSA_3_Graphs/Graphs_1_Creation_And_Basics.java
@@ -1,8 +1,6 @@
 package DSA_3_Graphs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Graphs_1_Creation_And_Basics {
     //    This will help for the creation of the vertex
@@ -424,5 +422,198 @@ public class Graphs_1_Creation_And_Basics {
             }
         }
         return !(flag >= 2);
+    }
+
+    public ArrayList<ArrayList<String>> getAllConnectedComponents() {
+        HashMap<String, Boolean> processed = new HashMap<>();
+//        This is for the queue purpose
+        LinkedList<Pair> queue = new LinkedList<>();
+        ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+//        To get all the connected components
+        ArrayList<ArrayList<String>> allConnectedComponents = new ArrayList<>();
+
+        for (String key : keys) {
+            if (processed.containsKey(key)){
+                continue;
+            }
+            Pair initialPair = new Pair();
+            initialPair.vertexName = key;
+            initialPair.pathSoFar = key;
+
+//            If there is no connection then the below code will only execute for once
+            ArrayList<String> newConnectedComponentGenerated = new ArrayList<>();
+
+//        Put the initial pair in the queue
+            queue.addLast(initialPair);
+//        Till the queue is not empty
+            while (!queue.isEmpty()) {
+                Pair removedPair = queue.removeFirst();
+//            If the node is already present in the processed Hashmap then no need to
+//            keep that node again in the hashmap
+
+                if (processed.containsKey(removedPair.vertexName)) {
+                    continue;
+//                Due to the usage of to continue all the below statements will not
+//                execute and the execution goes on.
+                }
+//            Put in the processed Hashmap
+                processed.put(removedPair.vertexName, true);
+
+//              Add vertex new ArrayList
+                newConnectedComponentGenerated.add(removedPair.vertexName);
+
+//            If the direct edge is not there then go to the neighbours of that
+//            and check for the path
+                Vertex neighboursVertexChunkLocation = vtces.get(removedPair.vertexName);
+                ArrayList<String> neighbours = new ArrayList<>(neighboursVertexChunkLocation.nbrs.keySet());
+                for (String nbr : neighbours) {
+//                If it is not processed then only add to the processed array
+                    if (!processed.containsKey(nbr)) {
+//                    Making the new pair of neighbour and put in the queue
+                        Pair newPair = new Pair();
+                        newPair.vertexName = nbr;
+                        newPair.pathSoFar = removedPair.pathSoFar + nbr;
+                        queue.addLast(newPair);
+                    }
+                }
+            }
+//            The final answer
+            allConnectedComponents.add(newConnectedComponentGenerated);
+        }
+        return allConnectedComponents;
+    }
+
+//    This part will now be used for Kruskal Algorithm
+    public static class DisjointSet{
+//        To store the nodes name and the referencing address of it.
+        HashMap<String, Node> map = new HashMap<>();
+//        Node to store data
+        private static class Node{
+            String data;
+            int rank; //Height
+            Node parent; // Self - referencing
+        }
+//        Function required for disjoint DSA
+        public void create(String value){
+//            Node structure is required
+            Node newNode = new Node();
+            newNode.data = value;
+//            Initial creation so rank would be zero
+            newNode.rank = 0;
+//            Self-referencing
+            newNode.parent = newNode;
+            map.put(value, newNode);
+        }
+
+//        Find operation
+        public void union(String value1, String value2){
+            Node set1 = map.get(value1);
+            Node set2 = map.get(value2);
+
+//            Now, we have to find parent of it
+//            That is the representative element
+            Node repElem1 = find(set1);
+            Node repElem2 = find(set2);
+
+//            Check that they are of same parent
+//            To avoid null safety
+            if (Objects.equals(repElem1.data, repElem2.data)){
+                return;
+            }
+            else {
+//                Rank changing
+                if (repElem1.rank == repElem2.rank){
+//                    Both have the same rank
+                    repElem2.parent = repElem1;
+                    repElem1.rank = repElem1.rank + 1;
+                }
+                else if (repElem1.rank > repElem2.rank){
+                    repElem2.parent = repElem1;
+                }
+                else {
+                    repElem1.parent = repElem2;
+                }
+            }
+        }
+
+//        Find the representative element
+//        A
+//       / \
+//      B   C
+//     /
+//    D
+//    D belongs to which set ?, So it gives representative element
+//    Hashmap to store Node name and the address of it
+        public String find(String value){
+            return find(map.get(value)).data;
+//            The storage is like...
+//            "A" : 10058 Memory location
+//            So, now A, data we need
+        }
+
+//        To obtain the node.
+        private Node find(Node node){
+//            When the node address and the node.parent is same
+            if (node == node.parent){
+                return node;
+            }
+            return find(node.parent);
+        }
+    }
+
+    private static class EdgePair implements Comparable<EdgePair>{
+        String value1;
+        String value2;
+        int cost;
+
+        @Override
+        public int compareTo(EdgePair e){
+            return this.cost - e.cost;
+        }
+
+        public String toString(){
+            return value1 + "-" + value2 + " : " + cost;
+        }
+    }
+
+    private ArrayList<EdgePair> getAllEdges(){
+        ArrayList<EdgePair> edgesList = new ArrayList<>();
+        for (String vertexName : vtces.keySet()){
+            Vertex vertex = vtces.get(vertexName);
+            for (String nbr : vertex.nbrs.keySet()){
+                EdgePair edgePair = new EdgePair();
+                edgePair.value1 = vertexName;
+                edgePair.value2 = nbr;
+                edgePair.cost = vertex.nbrs.get(nbr);
+                edgesList.add(edgePair);
+            }
+        }
+        return edgesList;
+    }
+
+    public void kruskal(){
+        ArrayList<EdgePair> edges = getAllEdges();
+        Collections.sort(edges);
+        System.out.println("The sorted edged :-" + edges + "\n");
+//        Set creation
+        DisjointSet set = new DisjointSet();
+        for (String vertexName : vtces.keySet()){
+            set.create(vertexName);
+        }
+//        Traverse edges
+        for (EdgePair edgePair : edges){
+//            Check the same set or different set
+//            If belong to different set then do union else ignore
+            String rep1 = set.find(edgePair.value1);
+            String rep2 = set.find(edgePair.value2);
+
+            if (rep1.equals(rep2)){
+                continue;
+            }
+            else {
+                System.out.println(edgePair);
+                set.union(edgePair.value1, edgePair.value2);
+            }
+        }
     }
 }
